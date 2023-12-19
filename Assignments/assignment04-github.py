a967b54b8aa67769194147b2c72419a349d48ea1
pcsukumar <sukumar@outlook.ie>
2023-12-19 16:32:30 +0000

    Assignment04 - Testing with API key

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -1,2 +1,15 @@
 # This program will read a file from a repository and replace all the instances of the text "Andrew" with your 'Sukumar'.
 # The program then commit those changes and push the file back to the repository.
+
+import requests
+import json
+from config import config as cfg
+
+filename = "repos-private.json"
+
+url = 'https://api.github.com/repos/pcsukumar/aprivateone'
+
+apikey = cfg["githubkey"]
+response = requests.get(url, auth = ('token', apikey))
+
+print (response.status_code)
