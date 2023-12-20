26c557a8620cf4133f2bb874d7ec9690b2fdfec0
pcsukumar <sukumar@outlook.ie>
2023-12-20 09:22:57 +0000

    Assignment04 - Getting clone URL

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -1,15 +1,20 @@
 # This program will read a file from a repository and replace all the instances of the text "Andrew" with your 'Sukumar'.
 # The program then commit those changes and push the file back to the repository.
 
-import requests
-import json
+from github import Github
 from config import config as cfg
 
-filename = "repos-private.json"
-
-url = 'https://api.github.com/repos/pcsukumar/aprivateone'
-
 apikey = cfg["githubkey"]
-response = requests.get(url, auth = ('token', apikey))
 
-print (response.status_code)
+g = Github(apikey)
+
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+
+repo =g.get_repo('pcsukumar/aprivateone')
+
+print (repo.clone_url)
+
+
+
+
